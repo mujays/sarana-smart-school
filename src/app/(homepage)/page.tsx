16d95b21317eb46c9b5327c225137b8cc1506b66
mujiburrohman <muji.official06@gmail.com>
2024-04-29 09:11:23 +0700

    setup project

---
+++ src/app/(homepage)/page.tsx
@@ -0,0 +1,6 @@
+export { default } from "@/components/pages/homepage";
+
+export const metadata = {
+  description: "Home Page",
+  title: "Home Page",
+};
---
+++ src/components/pages/homepage/components/navbar.tsx
@@ -0,0 +1,20 @@
+function Navbar() {
+  return (
+    <nav className="flex pl-5 md:pl-20">
+      {/* <Image
+        width={500}
+        height={300}
+        priority
+        alt="Logo SD"
+        src="/images/logo.png"
+        style={{
+          width: "170px",
+          height: "auto",
+        }}
+      />
+      <div className="bg-[#DBEFE1] w-full">test</div> */}
+    </nav>
+  );
+}
+
+export default Navbar;
